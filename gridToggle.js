acdfba1d88943f09ed5d5cd0e96c641abaef4235
Kyle Doliente <kdolient@uci.edu>
2021-08-07 02:54:55 -0700

    Image resizing

--- gridToggle.js
+++ gridToggle.js
@@ -1,5 +1,17 @@
-function changeRight(){
+function image1Focus(){
 	let link = document.getElementsByClassName("right-column")[0];
 	console.log(link);
-	link.innerHTML = "test";
+	let image = link.getElementsByClassName("image1");
+	image[0].style.gridColumnStart = '1';
+	image[0].style.gridColumnEnd = "3";
+
+	//image[0].setAttribute("onClick", "resetImage1Size()");
+}
+
+function resetImage1Size(){
+	let link = document.getElementsByClassName("right-column")[0];
+	console.log("oh");
+	let image = link.getElementsByClassName("image1");
+	image[0].style.gridColumnStart = '1';
+	image[0].style.gridColumnEnd = "2";
 }
